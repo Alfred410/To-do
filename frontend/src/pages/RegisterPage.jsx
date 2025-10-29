91ae451af4a1718ec53e95c0a6c412da0c9d9f82
Alfred410 <alfred.larsson@iths.se>
2025-10-29 15:42:04 +0100

    Edit regsiter page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,3 +1,54 @@
 export default function Regsiter() {
-  return <div>Register</div>;
+  return (
+    <div className="flex flex-col justify-center mt-5 px-4">
+      <h1 className="text-[37px] text-gray-800  text-center mb-9 mt-3">
+        Register
+      </h1>
+      <form className="flex items-center  text-black flex-col ">
+        <input
+          type="text"
+          placeholder="Firstname"
+          className="my-2 border rounded py-1 pl-2"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Lastname"
+          className="my-2 border rounded py-1 pl-2"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="my-2 border rounded py-1 pl-2"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="my-2 border rounded py-1 pl-2"
+          required
+        />
+        <div className="flex justify-center w-[250px] my-3">
+          <input
+            type="checkbox"
+            className="mr-2 border rounded py-1 pl-2"
+            required
+          />
+          <p className="text-sm text-gray-500">
+            I have read and accpet the{' '}
+            <a href="/" className="text-blue-400">
+              privacy policy
+            </a>
+          </p>
+        </div>
+        <button className="bg-indigo-600 text-white h-8 w-[100px] rounded mt-[150px] sm:mt-[50px]">
+          Register
+        </button>
+      </form>
+      <a href="/login" className="text-center mt-3">
+        Login here
+      </a>
+    </div>
+  );
 }
