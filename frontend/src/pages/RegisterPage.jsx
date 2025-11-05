d87013b8cd02b19b5d984978e2cb6cbbd77db7b6
Alfred410 <alfred.larsson@iths.se>
2025-11-05 13:53:00 +0100

    Register to database

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,35 +1,73 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { register } from '../services/userService';
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [feedback, setFeedback] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await register(firstName, lastName, email, password);
+      setFeedback({ type: 'success', message: 'Registrering lyckades' });
+      navigate('/login');
+    } catch (err) {
+      setFeedback({ type: 'error', message: err.message });
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center mt-5 px-4">
       <h1 className="text-[37px] text-gray-800  text-center mb-9 mt-3">
         Registrera dig
       </h1>
-      <form className="flex items-center  text-black flex-col ">
+      {feedback && (
+        <div
+          className={`p-2 rounded w-[210px] text-center  ${
+            feedback.type === 'error'
+              ? 'bg-red-100 text-red-700'
+              : 'bg-green-100 text-green-800'
+          }`}
+        >
+          {feedback.message}
+        </div>
+      )}
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center  text-black flex-col "
+      >
         <input
           type="text"
           placeholder="Förnamn"
           className="my-2 sm:flex-1 border border-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           required
+          onChange={(e) => setFirstName(e.target.value)}
         />
         <input
           type="text"
           placeholder="Efternamn"
           className="my-2 sm:flex-1 border border-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           required
+          onChange={(e) => setLastName(e.target.value)}
         />
         <input
           type="email"
           placeholder="E-post"
           className="my-2 sm:flex-1 border border-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           required
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Lösenord"
           className="my-2 sm:flex-1 border border-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           required
+          onChange={(e) => setPassword(e.target.value)}
         />
         <div className="flex justify-center w-[250px] my-3">
           <input
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,8 +1,9 @@
 const API_URL = `http://localhost:3000/api/user`;
 
+//LOGIN
 export async function login(email, password) {
   try {
-    const res = await fetch('http://localhost:3000/api/user/login', {
+    const res = await fetch(`${API_URL}/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -22,6 +23,28 @@ export async function login(email, password) {
     return data;
   } catch (err) {
     console.error('Serverfel', err);
+    throw err;
+  }
+}
+
+//REGISTER
+export async function register(firstName, lastName, email, password) {
+  try {
+    const res = await fetch(`${API_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ firstName, lastName, email, password }),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.message || 'Registering misslyckades');
+    }
+    return data;
+  } catch (err) {
+    console.error('Serverfel vid registrering', err);
     throw err;
   }
 }
