217601e3257b986926796899cca47d472de8c38d
Louise Nensen Lagnefors <nensen-lagnefors@hotmail.com>
2025-11-05 10:44:47 +0100

    Refactor Register: rename component and update language for links, buttons, headings and text

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,8 @@
-export default function Regsiter() {
+export default function Register() {
   return (
     <div className="flex flex-col justify-center mt-5 px-4">
       <h1 className="text-[37px] text-gray-800  text-center mb-9 mt-3">
-        Register
+        Registrera dig
       </h1>
       <form className="flex items-center  text-black flex-col ">
         <input
@@ -36,18 +36,18 @@ export default function Regsiter() {
             required
           />
           <p className="text-sm text-gray-500">
-            I have read and accpet the{' '}
+            Jag har läst och accepterat{' '}
             <a href="/" className="text-blue-400">
-              privacy policy
+              integritetspolicy
             </a>
           </p>
         </div>
         <button className="bg-indigo-600 text-white h-8 w-[100px] rounded mt-[150px] sm:mt-[50px]">
-          Register
+          Registrera
         </button>
       </form>
       <a href="/login" className="text-center text-blue-400 text-sm mt-3">
-        Login here
+        Logga in här
       </a>
     </div>
   );
