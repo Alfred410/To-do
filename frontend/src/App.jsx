887e7329366c65d1487892fbd81cddd91010f615
Alfred410 <alfred.larsson@iths.se>
2025-11-10 16:08:02 +0100

    delete lazyloading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-const Home = lazy(() => import('./pages/Home'));
-const Login = lazy(() => import('./pages/LoginPage'));
-const Register = lazy(() => import('./pages/RegisterPage'));
-const Profile = lazy(() => import('./pages/ProfilePage'));
-const Privacy = lazy(() => import('./pages/PrivacyPage'));
+// const Home = lazy(() => import('./pages/Home'));
+// const Login = lazy(() => import('./pages/LoginPage'));
+// const Register = lazy(() => import('./pages/RegisterPage'));
+// const Profile = lazy(() => import('./pages/ProfilePage'));
+// const Privacy = lazy(() => import('./pages/PrivacyPage'));
+import Home from './pages/Home';
+import Login from './pages/LoginPage';
+import Register from './pages/RegisterPage';
+import Profile from './pages/ProfilePage';
+import Privacy from './pages/PrivacyPage';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -15,17 +20,16 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      {/* <Suspense fallback={<Loader />}>    </Suspense> */}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
