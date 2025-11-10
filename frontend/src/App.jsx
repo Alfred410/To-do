69a668f4d7f76c80da2e1ebefc6db5263e4436e3
Alfred410 <alfred.larsson@iths.se>
2025-11-10 15:07:29 +0100

    Performance for app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,31 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/LoginPage';
-import Register from './pages/RegisterPage';
-import Profile from './pages/ProfilePage';
-import Privacy from './pages/PrivacyPage';
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/LoginPage'));
+const Register = lazy(() => import('./pages/RegisterPage'));
+const Profile = lazy(() => import('./pages/ProfilePage'));
+const Privacy = lazy(() => import('./pages/PrivacyPage'));
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
+import { lazy, Suspense } from 'react';
+
+const Loader = () => <div>Loading...</div>;
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
